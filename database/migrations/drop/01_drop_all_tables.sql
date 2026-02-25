0112e8b70a67380ab8de45ad801c10eeda6b3807
Kaya <176020798+Cotinga159@users.noreply.github.com>
2026-02-25 14:53:44 +0100

    chore(database): add DROP IF EXISTS statements before table creation

---
+++ database/migrations/drop/01_drop_all_tables.sql
@@ -0,0 +1,12 @@
+-- ============================================================================
+-- SUPPRESSION DE TOUTES LES TABLES DE LA BASE DE DONNEES
+
+-- Ajoutez ici les tables que vous souhaitez supprimer de la base de données
+-- ============================================================================
+
+DROP TABLE IF EXISTS favorites CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS recipes CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
---
+++ database/migrations/drop/02_drop_all_types.sql
@@ -0,0 +1,7 @@
+-- ============================================================================
+-- SUPPRESSION DE TOUTES LES TYPES DE LA BASE DE DONNEES
+
+-- Ajoutez ici les types que vous souhaitez supprimer de la base de données
+-- ============================================================================
+
+DROP TYPE IF EXISTS role_enum CASCADE;
