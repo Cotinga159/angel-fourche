ff6042c3798509cc6283df9c6211b22ffa555cfa
Kaya <176020798+Cotinga159@users.noreply.github.com>
2026-04-29 09:54:48 +0200

    fix: add IF NOT EXISTS for role_enum type

--- database/migrations/config/04_add_types.sql
+++ database/migrations/config/04_add_types.sql
@@ -1,9 +1,9 @@
 SET CLIENT_ENCODING TO 'UTF8';
 
-CREATE TYPE role_enum AS ENUM(
-    'admin',
-    'member'
-);
+DO $$ BEGIN
+    CREATE TYPE role_enum AS ENUM('admin', 'member');
+EXCEPTION WHEN duplicate_object THEN NULL;
+END $$;
 
 COMMENT ON TYPE role_enum IS 'Rôles des utilisateurs pour la gestion des droits Access Controle level (ACL)';
 
